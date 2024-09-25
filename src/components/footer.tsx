b97cbad19f13f9b1f3f08eee7114ed25b0b73b5c
minahilf <sminahilfatima@gmail.com>
2024-09-24 19:17:25 -0700

    Initial commit

---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+// export default function Footer(){
+//     return(
+//         <h1>Hello from footer</h1>
+//     )
+// }
+
+export  function Footer(){
+    return(
+        <h1>Hello from footer</h1>
+    )
+}
---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+// export default function Header(){
+//     return(
+//         <h1>Hello from header</h1>
+//     )
+// }
+
+export function Header(){
+    return(
+        <h1>Hello from header</h1>
+    )
+}
